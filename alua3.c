bd765a91cd894148d4f490cafc1b309769fd9cb2
monge95 <pedrokamonge@gmail.com>
2025-02-19 10:55:42 -0300

    Create alua3.c
    aula3

---
+++ alua3.c
@@ -0,0 +1,82 @@
+
+
+#include<stdio.h>
+
+int main()
+{
+/**
+ * 
+ * ex1
+    float not1;
+ printf("digite a nota 1 com a virgula");
+ scanf ("%f",&not1) ;
+     float not2;
+ printf("digite a nota 2 com a virgula");
+ scanf ("%f",&not2);
+
+ printf ("sua nota é %f", (not1 + not2)/2.0 );
+
+
+ ex 2
+  float km;
+ printf("digite a kilometragem que o percoreu");
+ scanf ("%f",&km) ;
+     float litros;
+ printf("digite a o consumo de litros do carro");
+ scanf ("%f",&litros);
+
+ printf ("seu carro faz %fkm por litro ",km/ litros );
+    
+    ex3 
+   int pagamentoHomen = 20;
+   int pagamentoMulher = 17;
+   int homen;
+ printf("digite quantos homens entraram na festa ");
+ scanf ("%d",&homen) ;
+     int mulher;
+ printf("digite digite quantas mulheres entraram na festa");
+ scanf ("%d",&mulher);
+
+ printf ("sua festa arrecadou %d",(homen*pagamentoHomen)+(mulher*pagamentoMulher));
+  
+  
+  ex4
+   float pi = 3.14;
+ float raio;
+ printf("digite o raio");
+ scanf ("%f",&raio) ;
+     float altura;
+ printf("digite a altura");
+ scanf ("%f",&altura);
+
+ printf ("o volume do cilindro é %f ",pi*(raio*raio)*altura );
+  
+  ex5
+      float a;
+ printf("digite ovalor a com as virgulas");
+ scanf ("%f",&a) ;
+ float b;
+ printf("digite ovalor b com as virgulas");
+ scanf ("%f",&b) ;
+     float c;
+ printf("digite ovalor c com as virgulas");
+ scanf ("%f",&c);
+
+ printf ("o volume do cilindro é %f ",(b*b)-4*a*c )
+ 
+  
+  int numero;
+printf("digite um numero inteiro");
+ scanf("%d",&numero);
+if(numero %2 = 0){
+    printf("seu numero é par");
+} else{
+    printf("seu numero é inpar")
+}
+  **/
+  
+  
+  
+  
+    return 0;
+}
