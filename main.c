9a18701871753f55195344f9ec5ea2c635103448
monge95 <pedrokamonge@gmail.com>
2025-02-19 21:16:58 -0300

    aula 3 exercicios

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-
-
 #include<stdio.h>
 
 int main()
@@ -8,15 +6,16 @@ int main()
  * 
  * ex1
     float not1;
- printf("digite a nota 1 com a virgula");
+  ("digite a nota 1 com a virgula");
  scanf ("%f",&not1) ;
      float not2;
  printf("digite a nota 2 com a virgula");
  scanf ("%f",&not2);
 
  printf ("sua nota é %f", (not1 + not2)/2.0 );
-
-
+**/
+/**
+ * 
  ex 2
   float km;
  printf("digite a kilometragem que o percoreu");
@@ -26,7 +25,10 @@ int main()
  scanf ("%f",&litros);
 
  printf ("seu carro faz %fkm por litro ",km/ litros );
+    **/
     
+   /**
+ *  
     ex3 
    int pagamentoHomen = 20;
    int pagamentoMulher = 17;
@@ -38,8 +40,9 @@ int main()
  scanf ("%d",&mulher);
 
  printf ("sua festa arrecadou %d",(homen*pagamentoHomen)+(mulher*pagamentoMulher));
-  
-  
+  **/
+  /**
+ 
   ex4
    float pi = 3.14;
  float raio;
@@ -50,8 +53,9 @@ int main()
  scanf ("%f",&altura);
 
  printf ("o volume do cilindro é %f ",pi*(raio*raio)*altura );
+  **/
   
-  ex5
+  /**
       float a;
  printf("digite ovalor a com as virgulas");
  scanf ("%f",&a) ;
@@ -62,21 +66,45 @@ int main()
  printf("digite ovalor c com as virgulas");
  scanf ("%f",&c);
 
- printf ("o volume do cilindro é %f ",(b*b)-4*a*c )
- 
+ printf ("o valor delta é  %f ",(b*b)-4*a*c );
+**/
   
+/**
   int numero;
 printf("digite um numero inteiro");
  scanf("%d",&numero);
-if(numero %2 = 0){
+if(numero %2 <= 0){
     printf("seu numero é par");
 } else{
-    printf("seu numero é inpar")
+    printf("seu numero é inpar");
 }
-  **/
+**/
+/**
+ * 
+ex 7
+ int f = 32;
+ int c;
+  printf("digite a temperatura em celcios");
+  scanf("%d", &c);
   
-  
-  
+  printf("a temperatura em farenheits é %f", (c*9.0/5.0)+f);
+   **/
+   /**
+ * 
+   float a;
+   float b;
+   float c;
+   
+   printf("coloque um valor com virgula");
+   scanf("%f", &a);
+   printf("coloque um valor com virgula");
+   scanf("%f",&b );
+   printf("coloque um valor com virgula");
+   scanf("%f", &c);
+   
+   printf("a média aritmética desses valores é %f", (a+b+c)/3.0);
+   
+   **/
   
     return 0;
 }
